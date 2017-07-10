c5f81cec7b1d52781ccd64849fa1df8fb7357164
StarTux <ths.loy@googlemail.com>
2017-07-10 22:26:03 +0200

    Make this useful

--- src/main/java/com/winthier/fish/FishHeadEntity.java
+++ src/main/java/com/winthier/fish/FishHeadEntity.java
@@ -15,8 +15,11 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
@@ -46,8 +49,8 @@ public final class FishHeadEntity implements CustomEntity, TickableEntity {
     public Entity spawnEntity(Location location) {
         return location.getWorld().spawn(location, ArmorStand.class, as -> {
                 as.setVisible(false);
+                as.setSmall(random.nextInt(3) > 0);
                 as.setMarker(true);
-                as.setSmall(true);
                 as.setGravity(false);
                 as.getEquipment().setHelmet(fishHeads.get(random.nextInt(fishHeads.size())).clone());
             });
@@ -64,6 +67,12 @@ public final class FishHeadEntity implements CustomEntity, TickableEntity {
     }
 
     @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event, EntityContext context) {
+        event.setCancelled(true);
+        ((Watcher)context.getEntityWatcher()).remove();
+    }
+
+    @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event, EntityContext context) {
         event.setCancelled(true);
     }
@@ -71,6 +80,11 @@ public final class FishHeadEntity implements CustomEntity, TickableEntity {
     @EventHandler
     public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event, EntityContext context) {
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent event, EntityContext context) {
+        ((Watcher)context.getEntityWatcher()).scared = 5;
     }
 
     @Override
@@ -88,6 +102,8 @@ public final class FishHeadEntity implements CustomEntity, TickableEntity {
         private final ArmorStand entity;
         private final FishHeadEntity customEntity;
         private int ticks = 0;
+        private int cooldown = 20;
+        private int scared = 0;
 
         void remove() {
             entity.remove();
@@ -96,26 +112,59 @@ public final class FishHeadEntity implements CustomEntity, TickableEntity {
 
         void onTick() {
             ticks += 1;
-            if (ticks % 20 == 0) {
-                Block block = entity.getLocation().getBlock().getRelative(0, 1, 0);
-                if (!block.isLiquid()) {
-                    remove();
-                } else {
-                    BlockFace[] faces = {BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.UP};
-                    List<Block> blocks = new ArrayList<>();
-                    blocks.add(block);
-                    for (BlockFace face: faces) {
-                        Block rel = block.getRelative(face);
-                        if (rel.isLiquid()) {
+            cooldown -= 1;
+            if (cooldown > 0) return;
+            cooldown = 20 + random.nextInt(40);
+            double eyeHeight = entity.isSmall() ? 0.987 : 1.975;
+            Block block = entity.getLocation().add(0, eyeHeight, 0).getBlock();
+            if (!block.isLiquid()) {
+                remove();
+                return;
+            }
+            int playersNearby = 0;
+            for (Entity nearby: entity.getNearbyEntities(4, 4, 4)) {
+                if (nearby instanceof Player) {
+                    if (plugin.allowedGameModes.contains(((Player)nearby).getGameMode())) playersNearby += 1;
+                }
+            }
+            if (playersNearby > 0) scared = 5;
+            Block newBlock;
+            if (scared > 0 && block.getRelative(0, -1, 0).isLiquid() && block.getRelative(0, -2, 0).isLiquid()) {
+                scared -= 1;
+                newBlock = block.getRelative(0, -1, 0);
+            } else if (random.nextBoolean()) {
+                newBlock = block;
+            } else {
+                BlockFace[] faces = {BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST};
+                final List<Block> blocks = new ArrayList<>();
+                for (BlockFace face: faces) {
+                    Block rel = block.getRelative(face);
+                    if (rel.isLiquid()
+                        && rel.getRelative(0, -1, 0).isLiquid()
+                        && rel.getRelative(0, -2, 0).isLiquid()) {
+                        blocks.add(rel);
+                        if (rel.getRelative(0, -3, 0).isLiquid()) {
                             blocks.add(rel);
                         }
                     }
-                    Block newBlock = blocks.get(random.nextInt(blocks.size())).getRelative(0, -1, 0);
-                    Location loc = newBlock.getLocation().add(0.5, 0.9, 0.5);
-                    loc.setYaw(random.nextFloat() * 360.0f);
-                    entity.teleport(loc);
+                }
+                Block rel = block.getRelative(BlockFace.UP);
+                if (rel.isLiquid()) {
+                    blocks.add(rel);
+                    blocks.add(rel);
+                    blocks.add(rel);
+                }
+                if (blocks.isEmpty()) {
+                    newBlock = block;
+                } else {
+                    newBlock = blocks.get(random.nextInt(blocks.size()));
                 }
             }
+            Location loc = newBlock.getLocation().add(0.5 + random.nextDouble() * 0.3 - random.nextDouble() * 0.3,
+                                                      0.7 + random.nextDouble() * 0.3 - random.nextDouble() * 0.3 - eyeHeight,
+                                                      0.5 + random.nextDouble() * 0.3 - random.nextDouble() * 0.3);
+            loc.setYaw(random.nextFloat() * 360.0f);
+            entity.teleport(loc);
         }
     }
 }
--- src/main/java/com/winthier/fish/FishPlugin.java
+++ src/main/java/com/winthier/fish/FishPlugin.java
@@ -5,10 +5,14 @@ import com.winthier.custom.entity.EntityWatcher;
 import com.winthier.custom.event.CustomRegisterEvent;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import lombok.Getter;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
@@ -17,6 +21,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -24,13 +29,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
 public class FishPlugin extends JavaPlugin implements Listener {
-    private Score score;
     private int spawnFish;
     private final Random random = new Random(System.currentTimeMillis());
+    final Set<GameMode> allowedGameModes = EnumSet.of(GameMode.SURVIVAL, GameMode.ADVENTURE);
 
     @Override
     public void onEnable() {
@@ -42,13 +48,7 @@ public class FishPlugin extends JavaPlugin implements Listener {
         Player player = (Player)sender;
         String cmd = args.length > 0 ? args[0].toLowerCase() : null;
         if (cmd == null) {
-            List<String> hi = new ArrayList<>(score.getScores().keySet());
-            Collections.sort(hi, (a, b) -> Integer.compare(score.getScores().get(b), score.getScores().get(a)));
-            int i = 1;
-            sender.sendMessage("" + ChatColor.GREEN + ChatColor.BOLD + "Fishing Highscore");
-            for (String name: hi) {
-                sender.sendMessage(ChatColor.GREEN + "" + (i++) + ") " + name + " " + score.getScore(name));
-            }
+            sender.sendMessage("fish");
         } else {
             return false;
         }
@@ -57,64 +57,68 @@ public class FishPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onCustomRegister(CustomRegisterEvent event) {
+        reloadConfig();
+        saveDefaultConfig();
         event.addEntity(new FishHeadEntity(this));
         spawnFish = getConfig().getInt("SpawnFish");
-        score = new Score(this);
-        score.load();
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.LOWEST)
     public void onPlayerFish(PlayerFishEvent event) {
         if (event.getState() != PlayerFishEvent.State.CAUGHT_FISH) return;
-        if (event.getCaught() == null || !(event.getCaught() instanceof Item) || ((Item)event.getCaught()).getItemStack().getType() != Material.RAW_FISH) {
+        if (event.getCaught() == null || !(event.getCaught() instanceof Item)) {
             return;
         }
         int fishNearby = 0;
+        int playersNearby = 0;
         FishHeadEntity.Watcher fishHead = null;
         Location caughtLocation = event.getCaught().getLocation();
-        if (!caughtLocation.getWorld().getName().equals("Resource")) return;
         for (Entity nearby: event.getCaught().getNearbyEntities(16, 16, 16)) {
-            EntityWatcher ew = CustomPlugin.getInstance().getEntityManager().getEntityWatcher(nearby);
-            if (ew != null && ew instanceof FishHeadEntity.Watcher) {
-                fishNearby += 1;
-                if (nearby.getLocation().distance(event.getCaught().getLocation()) <= 2.0) {
-                    fishHead = (FishHeadEntity.Watcher)ew;
-                }
-            }
-        }
-        if (fishNearby < spawnFish) {
-            for (int i = 0; i < spawnFish - fishNearby; i += 1) {
-                Block block = event.getCaught().getLocation().getBlock();
-                for (int j = 0; j < 20; j += 1) {
-                    BlockFace[] faces = {BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST};
-                    List<Block> blocks = new ArrayList<>();
-                    blocks.add(block);
-                    for (BlockFace face: faces) {
-                        Block rel = block.getRelative(face);
-                        if (rel.isLiquid()) {
-                            blocks.add(rel);
-                        }
+            if (nearby instanceof Player) {
+                if (allowedGameModes.contains(((Player)nearby).getGameMode())) playersNearby += 1;
+            } else if (nearby instanceof ArmorStand) {
+                EntityWatcher ew = CustomPlugin.getInstance().getEntityManager().getEntityWatcher(nearby);
+                if (ew != null && ew instanceof FishHeadEntity.Watcher) {
+                    fishNearby += 1;
+                    if (nearby.getLocation().distance(event.getCaught().getLocation()) <= 1.0) {
+                        fishHead = (FishHeadEntity.Watcher)ew;
                     }
-                    block = blocks.get(random.nextInt(blocks.size()));
                 }
-                CustomPlugin.getInstance().getEntityManager().spawnEntity(block.getLocation(), FishHeadEntity.CUSTOM_ID);
-                getLogger().info("Fish spawned at " + block);
             }
         }
         if (fishHead != null) {
+            if (fishHead.getEntity().isSmall()) {
+                ((Item)event.getCaught()).setItemStack(new ItemStack(Material.EMERALD));
+            } else {
+                ((Item)event.getCaught()).setItemStack(new ItemStack(Material.DIAMOND));
+            }
             fishHead.getEntity().getWorld().spawnParticle(Particle.CRIT, fishHead.getEntity().getLocation(), 24, 0.5, 0.5, 0.5, 0);
             fishHead.getEntity().getWorld().playSound(fishHead.getEntity().getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
-            score.addScore(event.getPlayer().getName(), 10);
             fishHead.remove();
-            for (Entity nearby: event.getPlayer().getNearbyEntities(64, 64, 64)) {
-                if (nearby instanceof Player) {
-                    ((Player)nearby).sendMessage(ChatColor.GREEN + event.getPlayer().getName() + " caught a big fish and has " + score.getScore(event.getPlayer().getName()) + " points. See /fish");
-                }
-            }
-            event.getPlayer().sendMessage(ChatColor.GREEN + "You caught a big fish and has " + score.getScore(event.getPlayer().getName()) + " points. See /fish");
-        } else {
-            score.addScore(event.getPlayer().getName(), 1);
         }
-        score.save();
+        int fishToSpawn = spawnFish * playersNearby - fishNearby;
+        for (int i = 0; i < fishToSpawn; i += 1) {
+            Set<Block> done = new HashSet<>();
+            Block block = event.getCaught().getLocation().getBlock().getRelative(0, 1, 0);
+            if (!block.isLiquid()) block = block.getRelative(0, -1, 0);
+            if (!block.isLiquid() || !block.getRelative(0, -1, 0).isLiquid()) return;
+            for (int j = 0; j < 20; j += 1) {
+                BlockFace[] faces = {BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST};
+                List<Block> blocks = new ArrayList<>();
+                for (BlockFace face: faces) {
+                    Block rel = block.getRelative(face);
+                    if (!done.contains(rel)
+                        && rel.isLiquid()
+                        && rel.getRelative(0, -1, 0).isLiquid()
+                        && rel.getRelative(0, -2, 0).isLiquid()) {
+                        blocks.add(rel);
+                        done.add(rel);
+                    }
+                }
+                if (blocks.isEmpty()) return;
+                block = blocks.get(random.nextInt(blocks.size()));
+            }
+            CustomPlugin.getInstance().getEntityManager().spawnEntity(block.getLocation().add(0.5, -1.9, 0.5), FishHeadEntity.CUSTOM_ID);
+        }
     }
 }
